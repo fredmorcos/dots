9d1a6cdb87291d732a65f0ce306c17f28fcdcfe7
Fred Morcos <fred.morcos@gmail.com>
2019-04-17 08:02:42 +0200

    Emacs

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -54,7 +54,8 @@
        (global-set-key (kbd "M-<") #'symbol-overlay-jump-prev)
 
        ;; magit
-       (global-set-key (kbd "C-x g") 'magit-status)))))
+       (global-set-key (kbd "C-x g") #'magit-status)
+       (add-hook 'with-editor-mode-hook #'flyspell-mode)))))
 
 (defvar background-thread
   (make-thread
