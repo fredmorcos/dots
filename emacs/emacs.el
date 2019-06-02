9c458ebe9ef00d32b5bbdce9ccb77101c4f13b98
Fred Morcos <fred.morcos@gmail.com>
2019-06-02 18:18:39 +0200

    Emacs

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -81,7 +81,7 @@
   :ensure nil
 
   :custom-face
-  (default ((t (:font "Fira Mono 13"))))
+  (default ((t (:font "Fira Mono 11"))))
   (cursor ((t (:background "Gray30"))))
   (region ((t (:background "LightSteelBlue1"))))
   (mode-line-highlight ((t (:box (:line-width 1 :color "Gray40" :style nil)))))
