ed6b341f637f314a1df8f4956d8b13dd767e2ebd
Fred Morcos <fred.morcos@gmail.com>
2020-04-16 10:41:42 +0200

    Updates

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -8,17 +8,18 @@
 ;; (add-hook 'after-init-hook #'profiler-report)
 
 ;; A big contributor to startup times is garbage collection.
-(setq-default gc-cons-threshold most-positive-fixnum
-              gc-cons-percentage 0.6)
+(setq-default
+  gc-cons-threshold most-positive-fixnum
+  gc-cons-percentage 0.6)
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 (custom-set-variables
- '(tool-bar-mode nil)
- '(menu-bar-mode nil)
- '(scroll-bar-mode nil))
+  '(tool-bar-mode nil)
+  '(menu-bar-mode nil)
+  '(scroll-bar-mode nil))
 
 (custom-set-faces
- '(default ((t (:font "Monospace 11" :foreground "Gray25" :background "Gray96")))))
+  '(default ((t (:font "Monospace 11" :foreground "Gray25" :background "Gray96")))))
 
 (fset 'display-startup-echo-area-message 'ignore)
 (run-with-idle-timer 5 t #'garbage-collect)
@@ -29,25 +30,25 @@
 (advice-add #'x-apply-session-resources :override #'ignore)
 
 (setq-default custom-file (concat temporary-file-directory "emacs/custom.el")
-              file-name-handler-alist nil
-              auto-window-vscroll nil
-              vc-handled-backends '(Git)
-              recentf-auto-cleanup 'never
-              after-init-hook nil
-              package-enable-at-startup nil
-              package--init-file-ensured t
-              auto-save-list-file-prefix nil
+  file-name-handler-alist nil
+  auto-window-vscroll nil
+  vc-handled-backends '(Git)
+  recentf-auto-cleanup 'never
+  after-init-hook nil
+  package-enable-at-startup nil
+  package--init-file-ensured t
+  auto-save-list-file-prefix nil
 
-              ;; Resizing the Emacs frame can be a terribly expensive part of changing the
-              ;; font. By inhibiting this, we easily halve startup times with fonts that
-              ;; are larger than the system default.
-              frame-inhibit-implied-resize t
+  ;; Resizing the Emacs frame can be a terribly expensive part of changing the
+  ;; font. By inhibiting this, we easily halve startup times with fonts that
+  ;; are larger than the system default.
+  frame-inhibit-implied-resize t
 
-              url-privacy-level 'high
-              url-proxy-services '(("no_proxy" . "127.0.0.1"))
+  url-privacy-level 'high
+  url-proxy-services '(("no_proxy" . "127.0.0.1"))
 
-              use-package-always-defer t
-              use-package-always-ensure t
-              use-package-expand-minimally t)
+  use-package-always-defer t
+  use-package-always-ensure t
+  use-package-expand-minimally t)
 
 ;;; early-init.el ends here
