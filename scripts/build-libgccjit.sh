98922e0039576cb4d6e8bf95cb9a15b5fdf459c3
Fred Morcos <fm@fredmorcos.com>
2020-09-30 09:41:46 +0200

    Updates

--- scripts/build-libgccjit.sh
+++ scripts/build-libgccjit.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 ./configure --prefix="/home/fred/.local" --libexecdir="/home/fred/.local/lib" --with-system-zlib --with-linker-hash-style=gnu --enable-host-shared --enable-shared --enable-checking=release --enable-languages=jit --enable-linker-build-id --disable-multilib --disable-bootstrap --disable-libssp --disable-lto --disable-libquadmath --disable-liboffloadmic --disable-libada --disable-libsanitizer --disable-libquadmath-support --disable-libgomp --disable-libvtv
+
+make -j3
+make DESTDIR="/home/fred/.local" jit.install-common jit.install-info
