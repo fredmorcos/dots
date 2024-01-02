67ee4f7ba60aafefecab032ea891981535d7b319
Fred Morcos <fm@fredmorcos.com>
2024-01-02 14:25:34 +0100

    Emacs

---
+++ attic/20240101/emacs/qol.el
@@ -0,0 +1,90 @@
+;;; package --- QoL improvements -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(defun fm/replace-escapes ()
+ "Replace strange newline escapes with proper UNIX newlines."
+ (interactive)
+ (goto-char (point-min))
+ (while (search-forward "\\n" nil t) (replace-match (char-to-string ?\n) nil t))
+ (goto-char (point-min))
+ (while (search-forward "\\t" nil t) (replace-match (char-to-string ?\t) nil t))
+ (goto-char (point-min))
+ (while (search-forward "" nil t) (replace-match "" nil t)))
+
+(defun fm/move-line-up ()
+ "Move a line up."
+ (interactive)
+ (transpose-lines 1)
+ (forward-line -2))
+
+(defun fm/move-line-down ()
+ "Move a line down."
+ (interactive)
+ (forward-line 1)
+ (transpose-lines 1)
+ (forward-line -1))
+
+(defun fm/insert-pair (left right &optional region-only)
+ "Insert LEFT & RIGHT in text, or around it if REGION-ONLY is t."
+ (if (use-region-p)
+  (let ((begin (region-beginning))
+        (end (region-end)))
+   (progn
+    (goto-char begin)
+    (insert-char left)
+    (goto-char (+ 1 end))
+    (insert-char right)))
+  (progn
+   (insert-char left)
+   (when (not region-only)
+    (progn
+     (insert-char right)
+     (backward-char))))))
+
+(defun fm/insert-pair-curly ()
+ "Insert curly braces in or around text."
+ (interactive)
+ (fm/insert-pair ?\{ ?\} nil))
+
+(defun fm/insert-pair-parens ()
+ "Insert parenthesis in or around text."
+ (interactive)
+ (fm/insert-pair ?\( ?\) t))
+
+(defun fm/insert-pair-quote ()
+ "Insert single quotes in or around text."
+ (interactive)
+ (fm/insert-pair ?\' ?\' t))
+
+(defun fm/insert-pair-double-quotes ()
+ "Insert double quotes in or around text."
+ (interactive)
+ (fm/insert-pair ?\" ?\" t))
+
+(defun fm/insert-pair-backtick ()
+ "Insert back-ticks in or around text."
+ (interactive)
+ (fm/insert-pair ?\` ?\` t))
+
+(defun fm/generate-password ()
+ "Generate a password and insert it."
+ (interactive)
+ (shell-command "pwgen -c -n -y -s -B -1 34 1" (current-buffer)))
+
+(defun fm/insert-buffer-name ()
+ "Insert the buffer's filename."
+ (interactive)
+ (insert (buffer-name)))
+
+(defmacro fm/append (list element)
+ "Append ELEMENT to LIST."
+ `(progn
+   (eval-when-compile
+    (autoload '-insert-at "dash")
+    (autoload '-contains-p "dash"))
+   (when (not (-contains-p ,list ,element))
+    (setf ,list (-insert-at (length ,list) ,element ,list)))))
+
+(provide 'qol)
+;;; qol.el ends here
