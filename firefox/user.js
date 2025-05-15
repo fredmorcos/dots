000b428ee5a91140850f21f6dda8e590fc81f62c
Fred Morcos <fm@fredmorcos.com>
2025-05-15 12:59:45 +0200

    Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -2,3 +2,4 @@ user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
 user_pref("media.ffmpeg.vaapi.enabled", 1);
 user_pref("network.trr.excluded-domains", "open-xchange.com,dovecot.net");
 user_pref("dom.ipc.forkserver.enable", 1);
+user_pref("browser.tabs.hoverPreview.enabled", 0);
