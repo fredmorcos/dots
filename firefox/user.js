158105a4590e0d8f33e8d2b21351fdd5b4468117
Fred Morcos <fm@fredmorcos.com>
2025-09-02 14:46:27 +0200

    Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -4,3 +4,40 @@ user_pref("network.trr.excluded-domains", "open-xchange.com,dovecot.net");
 user_pref("dom.ipc.forkserver.enable", 1);
 user_pref("browser.tabs.hoverPreview.enabled", 1);
 user_pref("browser.tabs.hoverPreview.showThumbnails", 0);
+user_pref("privacy.resistFingerprinting", true);
+
+/* Disable Pocket */
+user_pref("extensions.pocket.api", "http://0.0.0.0/pocket");
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.showHome", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled", false);
+
+/* Start Page */
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.preonboarding.enabled", false);
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+
+/* Tracker Feeds */
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredCheckboxes", false);
+user_pref("browser.newtabpage.activity-stream.system.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.system.showSponsoredCheckboxes", false);
+
+user_pref("browser.newtabpage.activity-stream.feeds.adsfeeds", false);
+user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false);
+user_pref("browser.newtabpage.activity-stream.feeds.inferredpersonalizationfeed", false);
+user_pref("browser.newtabpage.activity-stream.feeds.places", false);
+user_pref("browser.newtabpage.activity-stream.feeds.recommendationprovider", false);
+user_pref("browser.newtabpage.activity-stream.feeds.system.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.system.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.system.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.trendingsearchfeed", false);
+user_pref("browser.newtabpage.activity-stream.feeds.wallpaperfeed", false);
+user_pref("browser.newtabpage.activity-stream.feeds.weatherfeed", false);
+
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSites", false);
