0ff1984fdd9ad0464275134a0fe822243c813861
Fred Morcos <fm@fredmorcos.com>
2026-01-27 15:01:21 +0100

    Emacs

--- emacs/init-macros.el
+++ emacs/init-macros.el
@@ -28,6 +28,12 @@
 
 (defalias 'after #'with-eval-after-load)
 
+;; Modes.
+
+(defun mode (ext mode)
+ "Autoload and enable MODE for file extension EXT."
+ (push `(,ext . ,mode) auto-mode-alist))
+
 ;;; Packages
 
 (defmacro packages (&rest pkgs)
@@ -52,72 +58,51 @@
   (add-to-list 'package-selected-packages pkg)))
 
 ;; Hooks.
-(defmacro hook-globals (hook &rest funcs)
- "Hook FUNCS into HOOK."
- `(progn
-   ,@(mapcar #'(lambda (func)
-                `(add-hook ,hook ,func))
-      funcs)))
 
-(defmacro hook-local (hook func)
- "Hook FUNC into HOOK locally."
- `(add-hook ,hook ,func nil t))
+;; (defmacro hook-globals (hook &rest funcs)
+;;  "Hook FUNCS into HOOK."
+;;  `(progn
+;;    ,@(mapcar #'(lambda (func)
+;;                 `(add-hook ,hook ,func))
+;;       funcs)))
 
-(defmacro hook-progn (hook &rest body)
- "Hook (lambda () BODY) to HOOK."
- `(add-hook ,hook (lambda () (progn ,@body))))
+;; (defmacro hook-local (hook func)
+;;  "Hook FUNC into HOOK locally."
+;;  `(add-hook ,hook ,func nil t))
+
+;; (defmacro hook-progn (hook &rest body)
+;;  "Hook (lambda () BODY) to HOOK."
+;;  `(add-hook ,hook (lambda () (progn ,@body))))
 
 ;; Bind keys.
 
-(defmacro im/key (key func &optional pkg)
- "Define KEY in PKG-KEYMAP to call FUNC from PKG."
- `(progn
-   (im/autoload ,func ,pkg)
-   (global-set-key (kbd ,key) #',func)))
+;; (defmacro im/key (key func &optional pkg)
+;;  "Define KEY in PKG-KEYMAP to call FUNC from PKG."
+;;  `(progn
+;;    (im/autoload ,func ,pkg)
+;;    (global-set-key (kbd ,key) #',func)))
 
-(defmacro im/key-local (key func keymap &optional pkg)
- "Define KEY in KEYMAP to call FUNC from PKG."
- `(progn
-   (im/autoload ,func ,pkg)
-   (eval-when-compile (defvar ,keymap))
-   (define-key ,keymap (kbd ,key) #',func)))
+;; (defmacro im/key-local (key func keymap &optional pkg)
+;;  "Define KEY in KEYMAP to call FUNC from PKG."
+;;  `(progn
+;;    (im/autoload ,func ,pkg)
+;;    (eval-when-compile (defvar ,keymap))
+;;    (define-key ,keymap (kbd ,key) #',func)))
 
-(defmacro im/key-interactive (key &rest body)
- "Define KEY to call an interactive lambda with BODY."
- `(global-set-key (kbd ,key) (lambda () (interactive) ,@body)))
+;; (defmacro im/key-interactive (key &rest body)
+;;  "Define KEY to call an interactive lambda with BODY."
+;;  `(global-set-key (kbd ,key) (lambda () (interactive) ,@body)))
 
-(defmacro im/key-disable (key keymap)
- "Disable a KEY binding from KEYMAP."
- `(progn
-   (eval-when-compile (defvar ,keymap))
-   (define-key ,keymap (kbd ,key) nil)))
+;; (defmacro im/key-disable (key keymap)
+;;  "Disable a KEY binding from KEYMAP."
+;;  `(progn
+;;    (eval-when-compile (defvar ,keymap))
+;;    (define-key ,keymap (kbd ,key) nil)))
 
-(defmacro im/key-remap (old-func new-func)
- "Remap a global key from OLD-FUNC to NEW-FUNC."
- `(progn
-   (global-set-key [remap ,old-func] #',new-func)))
-
-;; Diminish.
-
-(defun im/dim-helper (mode text)
- "Diminish MODE to TEXT helper."
- (let ((element (assoc mode minor-mode-alist))
-       (new-text (if text (concat " " text) nil)))
-  (if element
-   (setf (nth 1 element) new-text)
-   (push `(,mode ,new-text) minor-mode-alist))))
-
-(defmacro im/dim (mode &optional text)
- "Diminish MODE to TEXT or nothing."
- `(im/dim-helper ',mode ,text))
-
-;; Modes.
-
-(defmacro im/mode (ext mode &optional pkg)
- "Autoload and enable MODE from PKG for file extension EXT."
- `(progn
-   (im/autoload ,mode ,pkg)
-   (push '(,(concat "\\" ext "\\'") . ,mode) auto-mode-alist)))
+;; (defmacro im/key-remap (old-func new-func)
+;;  "Remap a global key from OLD-FUNC to NEW-FUNC."
+;;  `(progn
+;;    (global-set-key [remap ,old-func] #',new-func)))
 
 (provide 'init-macros)
 ;;; init-macros.el ends here
