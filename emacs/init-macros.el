f8b21b6346f39d77a00a0164ed893de2b296ec6a
Fred Morcos <fm@fredmorcos.com>
2026-02-02 11:51:24 +0100

    Emacs

--- emacs/init-macros.el
+++ emacs/init-macros.el
@@ -45,64 +45,16 @@
 
 (defun package-setup (pkg)
  "Install PKG and add it to list of selected packages."
- (defvar *init/packages-refreshed* nil)
- (when (not (package-installed-p pkg))
-  (when (not *init/packages-refreshed*)
+ (unless (package-installed-p pkg)
+  (declvar package-archive-contents)
+  (unless package-archive-contents
    (message "+++ Refreshing package repositories")
-   (package-refresh-contents t)
-   (setq *init/packages-refreshed* t))
+   (package-refresh-contents t))
   (message "+++ Installing %s..." pkg)
   (package-install pkg))
  (after 'package
   (declvar package-selected-packages)
   (add-to-list 'package-selected-packages pkg)))
-
-;; Hooks.
-
-;; (defmacro hook-globals (hook &rest funcs)
-;;  "Hook FUNCS into HOOK."
-;;  `(progn
-;;    ,@(mapcar #'(lambda (func)
-;;                 `(add-hook ,hook ,func))
-;;       funcs)))
-
-;; (defmacro hook-local (hook func)
-;;  "Hook FUNC into HOOK locally."
-;;  `(add-hook ,hook ,func nil t))
-
-;; (defmacro hook-progn (hook &rest body)
-;;  "Hook (lambda () BODY) to HOOK."
-;;  `(add-hook ,hook (lambda () (progn ,@body))))
-
-;; Bind keys.
-
-;; (defmacro im/key (key func &optional pkg)
-;;  "Define KEY in PKG-KEYMAP to call FUNC from PKG."
-;;  `(progn
-;;    (im/autoload ,func ,pkg)
-;;    (global-set-key (kbd ,key) #',func)))
-
-;; (defmacro im/key-local (key func keymap &optional pkg)
-;;  "Define KEY in KEYMAP to call FUNC from PKG."
-;;  `(progn
-;;    (im/autoload ,func ,pkg)
-;;    (eval-when-compile (defvar ,keymap))
-;;    (define-key ,keymap (kbd ,key) #',func)))
-
-;; (defmacro im/key-interactive (key &rest body)
-;;  "Define KEY to call an interactive lambda with BODY."
-;;  `(global-set-key (kbd ,key) (lambda () (interactive) ,@body)))
-
-;; (defmacro im/key-disable (key keymap)
-;;  "Disable a KEY binding from KEYMAP."
-;;  `(progn
-;;    (eval-when-compile (defvar ,keymap))
-;;    (define-key ,keymap (kbd ,key) nil)))
-
-;; (defmacro im/key-remap (old-func new-func)
-;;  "Remap a global key from OLD-FUNC to NEW-FUNC."
-;;  `(progn
-;;    (global-set-key [remap ,old-func] #',new-func)))
 
 (provide 'init-macros)
 ;;; init-macros.el ends here
